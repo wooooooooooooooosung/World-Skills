64d49a6a024e432705751317c055424291c8e379
Woosung Jo <115052294+wooooooooooooooosung@users.noreply.github.com>
2023-09-26 18:15:59 +0900

    Create Login.java

---
+++ 2019_지방대회/src/pro/Login.java
@@ -0,0 +1,68 @@
+package pro;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends Frame {
+
+	JTextField jtt[] = { new JTextField(14), new JPasswordField(14) };
+	JButton jb[] = new JButton[3];
+
+	public Login() {
+		setFrame("로그인");
+
+		np.setLayout(new BorderLayout());
+
+		np.add(jl = new JLabel("STARBOX", JLabel.CENTER));
+		jl.setFont(new Font("Arial Black", Font.BOLD, 25));
+
+		cp.add(jp = new JPanel(new GridLayout(2, 1)), BorderLayout.WEST);
+		String ls[] = { "   ID : ", "PW : " };
+		for (int i = 0; i < 2; i++) {
+			jp.add(jp1 = new JPanel(new FlowLayout(1, 0, 5)));
+			jp1.add(new JLabel(ls[i], JLabel.RIGHT));
+		}
+
+		cp.add(jp = new JPanel(new GridLayout(2, 1)), BorderLayout.CENTER);
+		for (int i = 0; i < 2; i++) {
+			jp.add(jp1 = new JPanel(new FlowLayout(1, 0, 5)));
+			jp1.add(jtt[i]);
+		}
+
+		String bs[] = { "로그인", "회원가입", "종료" };
+		cp.add(jp = new JPanel(), BorderLayout.EAST);
+		jp.add(jb[0] = new JButton("<html><body><center><br>로그인<br><br></center></body></html>"));
+
+		cp.setBorder(BorderFactory.createEmptyBorder(10, 40, 10, 10));
+
+		sp.add(jp = new JPanel(new FlowLayout(1)));
+		for (int i = 0; i < bs.length; i++) {
+			if (i != 0) jp.add(jb[i] = new JButton(bs[i]));
+			jb[i].addActionListener(this);
+		}
+
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		showFrame();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(jb[0])) {
+			System.out.println("asd");
+		} else if (e.getSource().equals(jb[1])) {
+			System.out.println("asd");
+		} else if (e.getSource().equals(jb[2])) {
+			System.out.println("asd");
+		}
+	}
+}
