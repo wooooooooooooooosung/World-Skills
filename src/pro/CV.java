e447878b51b43437f62d4d216ec3e232c0590655
Woosung Jo <115052294+wooooooooooooooosung@users.noreply.github.com>
2023-10-09 22:05:45 +0900

    delete

--- src/pro/CV.java
+++
@@ -1,9 +0,0 @@
-package pro;
-
-public class CV {
-	public static String p_no = "1";
-	public static String p_name = "강ㄴㄴ";
-	public static String p_id;
-	public static String p_pw;
-	public static String p_pd;
-}
